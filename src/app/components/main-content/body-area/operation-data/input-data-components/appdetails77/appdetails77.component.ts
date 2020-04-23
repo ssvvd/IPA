0ef34473897d1defa4f0185ee97c3cf62a2129f5
SV <svetlanad@iscar.co.il>
2020-04-23 14:14:49 +0300

    77

--- src/app/components/main-content/body-area/operation-data/input-data-components/appdetails77/appdetails77.component.ts
+++ src/app/components/main-content/body-area/operation-data/input-data-components/appdetails77/appdetails77.component.ts
@@ -45,8 +45,7 @@ export class Appdetails77Component implements OnInit {
       this.SelectedDia =  this.arrdiameter.find(v=> v.Description==this.srv_StMng.IPL.GetItem('D_Hole').valuedefault)
       this.IsLoaded=true;              
       if(this.srv_StMng.SecAppSelected.MenuID=='111') this.srv_StMng.IPL.GetItem('IsRotating').value='false';
-      if(this.srv_StMng.SecAppSelected.MenuID=='112') this.srv_StMng.IPL.GetItem('IsRotating').value='true';
-      alert(this.srv_StMng.IPL.GetItem('IsRotating').value);
+      if(this.srv_StMng.SecAppSelected.MenuID=='112') this.srv_StMng.IPL.GetItem('IsRotating').value='true';      
 
       }
       )
