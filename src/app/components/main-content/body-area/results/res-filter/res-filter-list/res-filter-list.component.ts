6b3733eef8263233348f3b44ad62600269ec2848
HibaHawari <hibah@iscar.co.il>
2020-05-13 11:33:44 +0300

    flist

--- src/app/components/main-content/body-area/results/res-filter/res-filter-list/res-filter-list.component.ts
+++ src/app/components/main-content/body-area/results/res-filter/res-filter-list/res-filter-list.component.ts
@@ -150,66 +150,66 @@ export class ResFilterListComponent implements OnInit {
    
   }
  
-  callfunchttp(funcname:string)
-  {   
-    this.SpinnerService.show();
-    let str_param:string;
-    let str_s:string= this.str_search;
-    str_s =str_s.trim();
-    let t:number= this.Top;
-    if (str_s.replace(/\s/g, "") !='') t =9999 ; 
-    str_s=str_s.replace (' ' ,'blank');
-    if (str_s.replace(/\s/g, "") =='') str_s ='All';
+  // callfunchttp(funcname:string)
+  // {   
+  //   this.SpinnerService.show();
+  //   let str_param:string;
+  //   let str_s:string= this.str_search;
+  //   str_s =str_s.trim();
+  //   let t:number= this.Top;
+  //   if (str_s.replace(/\s/g, "") !='') t =9999 ; 
+  //   str_s=str_s.replace (' ' ,'blank');
+  //   if (str_s.replace(/\s/g, "") =='') str_s ='All';
 
-    //str_param=this.funcpar + str_s + "/" + t;
-    let param:string;
-    switch(funcname)
-    { 
-      case "BRAND": {
-          param=this.srv_appsetting.Units + "/1/1/" + this.srv_StMng.SecApp + "/"; 
-          str_param=param + str_s + "/" + t;
-          this.eventsSubscription.add(this.srv_dl_toolopt.td_brandname_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res);}) );
-          break; 
-      } 
-      case"TOOL":  {        
-          param=this.srv_StMng.IPL.GetItem("TD_BrandName").value + "/0/999/All/1/1/" + this.srv_StMng.SecApp +"/" + this.srv_appsetting.Units + "/All/";
-          str_param=param + str_s + "/" + t;
-          this.eventsSubscription.add(this.srv_dl_toolopt.td_tool_designation_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)}) );
-          break; 
-      }
-      case "INSERT":  {                 
-          param=this.srv_StMng.IPL.GetItem("TD_BrandName").value +"/" + this.srv_StMng.IPL.GetItem("TD_ToolDesignation").value+  "/0/999/1/1/" + this.srv_StMng.SecApp +"/" + this.srv_appsetting.Units + "/";
-          str_param=param + str_s + "/" + t;
-          this.eventsSubscription.add(this.srv_dl_toolopt.td_insert_designation_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)}) );
-          break; 
-      } 
-      case"TOOLCATALOGNO":  { 
-          param=this.srv_StMng.IPL.GetItem("TD_BrandName").value + "/0/999/All/1/1/" + this.srv_StMng.SecApp +"/" + this.srv_appsetting.Units + "/All/"  + this.srv_StMng.IPL.GetItem("TD_ToolDesignation").value+ "/";
-          str_param=param + str_s + "/" + t;
-          this.eventsSubscription.add(this.srv_dl_toolopt.td_tool_catalogno_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)}) );
-          break; 
-      }
-      case"INSERTCATALOGNO":  { 
-          param=this.srv_StMng.IPL.GetItem("TD_BrandName").value + "/" +this.srv_StMng.IPL.GetItem("TD_InsertDesignation").value + "/" 
-          + this.srv_appsetting.Units + "/1/1/All/" + this.srv_StMng.SecApp +"/" + this.srv_StMng.IPL.GetItem("TD_ToolDesignation").value+"/" + this.srv_StMng.IPL.GetItem("TD_ToolCatalogNo").value+ "/0/999/";
-          str_param=param + str_s + "/" + t;
-          this.eventsSubscription.add(this.srv_dl_toolopt.td_insert_catalogno_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)})) ;
-          break; 
-      }
-      case"GRADE":  {          
-          param=this.srv_StMng.IPL.GetItem("TD_BrandName").value + "/" +this.srv_StMng.IPL.GetItem("TD_InsertDesignation").value + "/1/1/" +
-          this.srv_StMng.IPL.GetItem("TD_InsertCatalogNo").value +"/"+ this.srv_appsetting.Units + "/" + this.srv_StMng.SecApp +"/" + 
-          this.srv_StMng.IPL.GetItem("TD_ToolDesignation").value+ "/0/999/";
-          str_param=param + str_s + "/" + t;
-          this.eventsSubscription.add(this.srv_dl_toolopt.td_grade_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)})) ;
-          break; 
-      }
-      default: {             
-          break; 
-      } 
-    }   
+  //   //str_param=this.funcpar + str_s + "/" + t;
+  //   let param:string;
+  //   switch(funcname)
+  //   { 
+  //     case "BRAND": {
+  //         param=this.srv_appsetting.Units + "/1/1/" + this.srv_StMng.SecApp + "/"; 
+  //         str_param=param + str_s + "/" + t;
+  //         this.eventsSubscription.add(this.srv_dl_toolopt.td_brandname_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res);}) );
+  //         break; 
+  //     } 
+  //     case"TOOL":  {        
+  //         param=this.srv_StMng.IPL.GetItem("TD_BrandName").value + "/0/999/All/1/1/" + this.srv_StMng.SecApp +"/" + this.srv_appsetting.Units + "/All/";
+  //         str_param=param + str_s + "/" + t;
+  //         this.eventsSubscription.add(this.srv_dl_toolopt.td_tool_designation_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)}) );
+  //         break; 
+  //     }
+  //     case "INSERT":  {                 
+  //         param=this.srv_StMng.IPL.GetItem("TD_BrandName").value +"/" + this.srv_StMng.IPL.GetItem("TD_ToolDesignation").value+  "/0/999/1/1/" + this.srv_StMng.SecApp +"/" + this.srv_appsetting.Units + "/";
+  //         str_param=param + str_s + "/" + t;
+  //         this.eventsSubscription.add(this.srv_dl_toolopt.td_insert_designation_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)}) );
+  //         break; 
+  //     } 
+  //     case"TOOLCATALOGNO":  { 
+  //         param=this.srv_StMng.IPL.GetItem("TD_BrandName").value + "/0/999/All/1/1/" + this.srv_StMng.SecApp +"/" + this.srv_appsetting.Units + "/All/"  + this.srv_StMng.IPL.GetItem("TD_ToolDesignation").value+ "/";
+  //         str_param=param + str_s + "/" + t;
+  //         this.eventsSubscription.add(this.srv_dl_toolopt.td_tool_catalogno_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)}) );
+  //         break; 
+  //     }
+  //     case"INSERTCATALOGNO":  { 
+  //         param=this.srv_StMng.IPL.GetItem("TD_BrandName").value + "/" +this.srv_StMng.IPL.GetItem("TD_InsertDesignation").value + "/" 
+  //         + this.srv_appsetting.Units + "/1/1/All/" + this.srv_StMng.SecApp +"/" + this.srv_StMng.IPL.GetItem("TD_ToolDesignation").value+"/" + this.srv_StMng.IPL.GetItem("TD_ToolCatalogNo").value+ "/0/999/";
+  //         str_param=param + str_s + "/" + t;
+  //         this.eventsSubscription.add(this.srv_dl_toolopt.td_insert_catalogno_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)})) ;
+  //         break; 
+  //     }
+  //     case"GRADE":  {          
+  //         param=this.srv_StMng.IPL.GetItem("TD_BrandName").value + "/" +this.srv_StMng.IPL.GetItem("TD_InsertDesignation").value + "/1/1/" +
+  //         this.srv_StMng.IPL.GetItem("TD_InsertCatalogNo").value +"/"+ this.srv_appsetting.Units + "/" + this.srv_StMng.SecApp +"/" + 
+  //         this.srv_StMng.IPL.GetItem("TD_ToolDesignation").value+ "/0/999/";
+  //         str_param=param + str_s + "/" + t;
+  //         this.eventsSubscription.add(this.srv_dl_toolopt.td_grade_list(str_param).subscribe((res: any)=>{this.filldatasubscribe(res)})) ;
+  //         break; 
+  //     }
+  //     default: {             
+  //         break; 
+  //     } 
+  //   }   
 
-  }
+  // }
 
   getmore()
   {        
@@ -232,78 +232,78 @@ export class ResFilterListComponent implements OnInit {
       }
   }
 
-  filldatasubscribe(data:any)
-  {   
-    if(data=='error') { this.SpinnerService.hide();return;}   
-    this.show_more = false;   
-    let n:number=0;
-    this.arrData =[];     
-    //add from post      
-    for (const d of JSON.parse(data)) {                                                     
-            if(d[this.fieldname]!='All' && d[this.fieldname].replace(/\s/g, "")!='')                                     
-              this.arrData.push({                
-                                  RecordID:n++,
-                                  Designation:d[this.fieldname],
-                                  Value:  d[this.fieldname],
-                                  Checked:false
-                                })          
-    }
-      //add selected
-      this.selitems.forEach(pp=>{
-      let a:ToolOptimizeItem[];
-      a=this.arrData.filter(p=>p.Designation==pp.Designation)
-      if(a.length==0)
-      {
-        this.arrData.unshift({                
-                        RecordID:pp.RecordID,
-                        Designation:pp[this.fieldname],
-                        Value:  pp[this.fieldname],
-                        Checked:true
-                        }) 
-      }
-      else    
-        a[0].Checked=true;      
-    } );
+  // filldatasubscribe(data:any)
+  // {   
+  //   if(data=='error') { this.SpinnerService.hide();return;}   
+  //   this.show_more = false;   
+  //   let n:number=0;
+  //   this.arrData =[];     
+  //   //add from post      
+  //   for (const d of JSON.parse(data)) {                                                     
+  //           if(d[this.fieldname]!='All' && d[this.fieldname].replace(/\s/g, "")!='')                                     
+  //             this.arrData.push({                
+  //                                 RecordID:n++,
+  //                                 Designation:d[this.fieldname],
+  //                                 Value:  d[this.fieldname],
+  //                                 Checked:false
+  //                               })          
+  //   }
+  //     //add selected
+  //     this.selitems.forEach(pp=>{
+  //     let a:ToolOptimizeItem[];
+  //     a=this.arrData.filter(p=>p.Designation==pp.Designation)
+  //     if(a.length==0)
+  //     {
+  //       this.arrData.unshift({                
+  //                       RecordID:pp.RecordID,
+  //                       Designation:pp[this.fieldname],
+  //                       Value:  pp[this.fieldname],
+  //                       Checked:true
+  //                       }) 
+  //     }
+  //     else    
+  //       a[0].Checked=true;      
+  //   } );
              
-    if(this.arrData.length <6) 
-    {
-      this.max_row_show =this.arrData.length;
-      this.show_more = false;
-    }
-    else
-    {
-      this.max_row_show = 5;
-      this.show_more = true;
-    }                                                                                                                            
-    this.SpinnerService.hide();
-    this.after_get_data =true;
-    this.arrDataF=this.arrData;
-    this.setSelectedItemsFromServerStateManager();
-    this.isshowdetail=true;        
-  }
+  //   if(this.arrData.length <6) 
+  //   {
+  //     this.max_row_show =this.arrData.length;
+  //     this.show_more = false;
+  //   }
+  //   else
+  //   {
+  //     this.max_row_show = 5;
+  //     this.show_more = true;
+  //   }                                                                                                                            
+  //   this.SpinnerService.hide();
+  //   this.after_get_data =true;
+  //   this.arrDataF=this.arrData;
+  //   this.setSelectedItemsFromServerStateManager();
+  //   this.isshowdetail=true;        
+  // }
   
-  setSelectedItemsFromServerStateManager()
-  {
-    if(this.srv_StMng.IPL!= null)
-     {
-       //this.srv_StMng.IPL.GetItem(this.fieldid).value;
-       let arr:string[];
-       arr = this.srv_StMng.IPL.GetItem(this.fieldid).value.split(',');      
-       for (let value of arr) {
-          if(this.arrDataF.filter(p=>p.Value==value).length>0)
-            this.arrData.filter(p=>p.Value==value)[0].Checked =true;
-      }     
-    }
-  }
+  // setSelectedItemsFromServerStateManager()
+  // {
+  //   if(this.srv_StMng.IPL!= null)
+  //    {
+  //      //this.srv_StMng.IPL.GetItem(this.fieldid).value;
+  //      let arr:string[];
+  //      arr = this.srv_StMng.IPL.GetItem(this.fieldid).value.split(',');      
+  //      for (let value of arr) {
+  //         if(this.arrDataF.filter(p=>p.Value==value).length>0)
+  //           this.arrData.filter(p=>p.Value==value)[0].Checked =true;
+  //     }     
+  //   }
+  // }
   
-  get_strselectedvalue(arr :ToolOptimizeItem[]):string
-  {
-    let str:string ='';     
-    arr.forEach(pp=>{str = str +pp.Designation + ",";} );
-    if(str!='') str=str.substring(0,str.length-1);
-    return str;
-    //str =str.substring (0,this.select_items.length-1);
-  }
+  // get_strselectedvalue(arr :ToolOptimizeItem[]):string
+  // {
+  //   let str:string ='';     
+  //   arr.forEach(pp=>{str = str +pp.Designation + ",";} );
+  //   if(str!='') str=str.substring(0,str.length-1);
+  //   return str;
+  //   //str =str.substring (0,this.select_items.length-1);
+  // }
 
   checkItem(value:string, event)
   {
