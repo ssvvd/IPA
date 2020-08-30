e6ab4959363d82a54ff1a6e3f5e3e6f729756f2e
HibaHawari <hibah@iscar.co.il>
2020-08-30 10:01:59 +0300

    brandName

--- src/app/components/main-content/body-area/results/results-table/results-table.component.ts
+++ src/app/components/main-content/body-area/results/results-table/results-table.component.ts
@@ -427,7 +427,7 @@ renderTable(res1:any, res2:any, res3:any, res4:any,res5:any, res6:any){
       this.dtResultsObjects3d[i][index] = []
       index3 = 0
      
-      if (this.dtResultsObjects[i][col1].property.FieldDescriptionSmall == 'Brand Name' || this.dtResultsObjects[i][col1].property.Field.toLowerCase().includes('listprice')){
+      if (this.dtResultsObjects[i][col1].property.Field.toLowerCase().includes('listprice')){
         let grpID:number = this.dtResultsObjects[i][col1].property.GroupID
         let order:number = this.dtResultsObjectsHelp[i].GroupID.indexOf(grpID, 0)
         index3 = order
@@ -457,7 +457,7 @@ renderTable(res1:any, res2:any, res3:any, res4:any,res5:any, res6:any){
     // {
     //   groupsOrder.push(this.dtResultsObjects[i][col2].property.GroupID)
     // }
-    if (this.dtResultsObjects[i][col2].property.FieldDescriptionSmall == 'Brand Name'  || this.dtResultsObjects[i][col1].property.Field.toLowerCase().includes('listprice')){
+    if (this.dtResultsObjects[i][col1].property.Field.toLowerCase().includes('listprice')){
       let grpID:number = this.dtResultsObjects[i][col2].property.GroupID
       let order:number = this.dtResultsObjectsHelp[i].GroupID.indexOf(grpID, 0)
       index3 = order
