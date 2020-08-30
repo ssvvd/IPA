21160c0bf1131ff9bdd4db32a48ebdd19499804b
HibaHawari <hibah@iscar.co.il>
2020-08-30 12:29:13 +0300

    matSaveHard

--- src/app/services/materialsm.service.ts
+++ src/app/services/materialsm.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { clsMaterial } from 'src/app/models/materials/material'
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +7,21 @@ import { Injectable } from '@angular/core';
 export class MaterialsmService {
 
   constructor() { }
+  private mainMaterialsTable:Map<string,clsMaterial[]>=new Map()
+
+
+  checkCategoryTableExists(lang:string,category:string){
+    return this.mainMaterialsTable.has(lang + category)
+  }
+
+  setNewCategoryTable(lang:string,category:string,data:clsMaterial[]){
+    this.mainMaterialsTable.set(lang + category,data)
+  }
+
+  getCategoryTable(lang:string,category:string){
+    return this.mainMaterialsTable.get(lang + category)
+  }
+
+
 }
    
