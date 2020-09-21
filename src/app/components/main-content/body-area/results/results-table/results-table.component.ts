44b2aaee568d091671a517ec711b653b5588d03a
HibaHawari <hibah@iscar.co.il>
2020-09-21 10:58:04 +0300

    filter

--- src/app/components/main-content/body-area/results/results-table/results-table.component.ts
+++ src/app/components/main-content/body-area/results/results-table/results-table.component.ts
@@ -57,6 +57,7 @@ export class ResultsTableComponent implements OnInit {
   sortProp:string="";
   countrow:number=0;
   showingrows:number=0;
+  lastTypeMainFilter:string="";
 
   @Input() filterChangedRec: any ;
   @Output() goToViewEvent = new EventEmitter<any>();
@@ -86,6 +87,7 @@ export class ResultsTableComponent implements OnInit {
   this.sortProp = 'index';
   this.ErrMsg = false
   this.countrow = 0
+  this.lastTypeMainFilter = "FilterRec"
   this.GetResult();
   }
 
@@ -849,6 +851,9 @@ ngOnChanges(changes:SimpleChanges) {
                 if (this.dtResultsObjectsHelp[i].AverageUse < 1)
                   this.dtResultsObjectsHelp[i].isHidden++
                   this.sortProp = 'AverageUse';
+                  if(this.lastTypeMainFilter == "FilterRec" && this.dtResultsObjectsHelp[i].IsExpand == "False"){
+                    this.dtResultsObjectsHelp[i].isHidden--
+                  }
                 break;
             }
             break;   
@@ -922,6 +927,12 @@ ngOnChanges(changes:SimpleChanges) {
     this.showingrows = this.dtResultsObjectsHelp.filter((obj) => obj.isHidden < 1).length;
     if (this.filterChangedRec.control == 'TypeFeed')
       this.lasTypeFeed = this.filterChangedRec.Res;
+    
+      if (this.filterChangedRec.control == 'optFilter')
+      this.lastTypeMainFilter = this.filterChangedRec.Res;
+
+      if (this.filterChangedRec.control == 'ClearAll')
+      this.lastTypeMainFilter = "FilterRec"
   }
   
 }
@@ -929,8 +940,12 @@ ngOnChanges(changes:SimpleChanges) {
 filterRecommended(prop:clsHelpProp){
     if (prop.IsExpand == "False")
         prop.isHidden++
-    else
-        prop.isHidden--
+    // else
+    //     prop.isHidden--
+
+        if(this.lastTypeMainFilter == "FilterSeller" && prop.AverageUse < 1){
+          prop.isHidden--
+        }
 }
 
 
